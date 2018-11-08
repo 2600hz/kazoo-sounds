db64a62f605e7580d1e5275734b3fea10031b721
James Aimonetti <james.aimonetti@gmail.com>
2018-11-08 19:31:18 +0000

    list missing

--- kazoo-core/list_missing.sh
+++ kazoo-core/list_missing.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-cd `dirname $0`
+pushd $(dirname $0) > /dev/null
+
+not_found=0
 
 while read i
-do 
-  FILENAME=`echo $i | cut -d':' -f1`; 
-  if [ -z "${FILENAME}" ]
-  then 
+do
+    FILENAME=`echo $i | cut -d':' -f1`;
+    if [ -z "${FILENAME}" ]
+    then
   	continue
-  fi
+    fi
 
-  for DIR in `find . -mindepth 2 -type d`
-  do
+    for DIR in `find . -mindepth 2 -type d`
+    do
 	if [ $(ls ${DIR}/${FILENAME}* 2>/dev/null | wc -w) -lt 1 ]
 	then
-	  echo ${DIR}/${FILENAME}
+	    echo ${DIR}/${FILENAME}
+            ((++not_found))
 	fi
-  done
-  
-done < prompts.txt 
+    done
+
+done < prompts.txt
+
+popd > /dev/null
+
+[[ $not_found > 0 ]]; echo "Found $not_found missing prompts"
+exit $not_found
